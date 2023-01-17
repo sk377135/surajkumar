2e46d66d5ff1ea6d59951f16d6af544b48d32dcb
suraj kumar <surajkumar.sk76@gmail.com>
2023-01-17 22:58:26 +0530

    added all files

---
+++ Boostrap/Day-2/import_Export/index.js
@@ -0,0 +1,12 @@
+//////////importing nav bar/////////////
+import navbar from './navbar.js';
+
+
+let container=document.querySelector("#container");
+container.innerHTML=navbar();
+
+
+
+// import navbar from "./navbar";
+// let container=document.getElementById(main);
+// container.innerHTML=navbar();
--- Boostrap/Day-2/import_Export/navbar.js
+++ Boostrap/Day-2/import_Export/navbar.js
@@ -1,15 +1,102 @@
- function navbar  () {
-    return 
-        ` <div id="navbar">
+function navbar(){
+    return`
+    <div id="navbar">
         <div>
-            <h1>Home</h1>
+            <h2 style="margin-bottom: 0px;">Flipkart</h2>
+            <a href="" style="margin-top: 0px;">Explore Plus</a>
+            
         </div>
-        <div>
-            <input type="text">
-            <button>Login</button>
-            <button>signUp</button>
+        <div style="display: flex; position: relative;">
+            <input type="text" placeholder="Search for products, brands and more">
+
+
+            <button style="background-color: transparent;  position: absolute; right: 5px;align-items: center; text-align: center; top: 5.8px; border: 0px;">🔍</button>
         </div>
-      </div>`;
-    
- }
- export default navbar();
+        <div >
+            <ul id="nav_content">
+                <li>Login</li>
+                <li>Become a seller</li>
+                <li>More</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    <div id="nav">
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Top Offers</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Mobiles & Tablets</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Electronics</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/904f3c8e7101408b.png?q=100" alt="">
+    </div>
+    <div>
+        <p>TVs & Appliances</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/7f7355480c6adc16.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Fashion</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/f97d6138516056bc.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Beauty</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/6728ed3cf145562c.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Home & Furniture</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=10" alt="">
+    </div>
+    <div>
+        <p>Flights</p>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/46376ceed3448aff.png?q=100" alt="">
+    </div>
+    <div>
+        <p>Grocery</p>
+    </div>
+</div>
+</div>
+    `
+
+}
+export default navbar;
+
