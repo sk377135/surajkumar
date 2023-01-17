361df180518168254dbc42ae903e7a98c4253122
suraj kumar <surajkumar.sk76@gmail.com>
2023-01-17 17:44:28 +0530

    get added

---
+++ Boostrap/Day-1/navbar.js
@@ -0,0 +1,54 @@
+// const navbar = () =>{
+//     return `
+//     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top " id="navbar-example2" >
+//     <a class="navbar-brand" href="#">Navbar</a>
+//     <div class="container-fluid" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0"> 
+//       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//         <span class="navbar-toggler-icon"></span>
+//       </button>
+//       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//         <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav nav-pills" >
+
+//           <li class="nav-item">
+//             <a class="nav-link "  href="#scrollspyHeading1">Home</a>
+//           </li>
+
+//           <li class="nav-item">
+//             <a class="nav-link" href="/surajkumar/Boostrap/Day-1/About.html">About</a>
+//           </li>
+
+
+//           <li class="nav-item dropdown nav-item ">
+//             <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+//             <ul class="dropdown-menu">
+//               <li><a class="dropdown-item" href="#scrollspyHeading4">My Village</a></li>
+//               <li><a class="dropdown-item" href="#scrollspyHeading5">My College</a></li>
+//               <li><hr class="dropdown-divider"></li>
+//               <li><a class="dropdown-item" href="#scrollspyHeading6">Banglore</a></li>
+//             </ul>
+//           </li>
+
+//           <li class="nav-item">
+//             <a class="nav-link" href="#scrollspyHeading3">Contect Us</a>
+//         </li>
+
+//           <!-- <li class="nav-item">
+//             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//           </li> -->
+//         </ul>
+//         <button type="button" class="btn btn-primary">
+//             Notifications <span class="badge bg-secondary">4</span>
+//           </button>
+//         <form class="d-flex">
+//           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+//           <button class="btn btn-outline-success" type="submit">Search</button>
+//           <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#LoginModal" >Login</button>
+//           <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#SignUpModal" >SignUp</button>
+//         </form>
+//       </div>
+//     </div>
+// </nav>
+
+//     `;
+// }
+// export default navbar;
---
+++ Boostrap/Day-2/import_Export/navbar.js
@@ -0,0 +1,15 @@
+ function navbar  () {
+    return 
+        ` <div id="navbar">
+        <div>
+            <h1>Home</h1>
+        </div>
+        <div>
+            <input type="text">
+            <button>Login</button>
+            <button>signUp</button>
+        </div>
+      </div>`;
+    
+ }
+ export default navbar();
